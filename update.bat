b1ba2a48831ebe1fd2a65eade02f8023af264599
ruraomsk <rusinovyury@gmail.com>
2018-10-10 14:25:23 +0600

    исправил батник

--- update.bat
+++ update.bat
@@ -4,13 +4,13 @@ rmdir /S /Q d:\md\pti\pr
 cd \md\pti\
 git clone ivangit@192.168.1.12:/home/ivangit/pr
 cd \combo\data
-copy \md\pti\pr\DU\DU.xml >null
-copy \md\pti\pr\Baz1\Baz1.xml >null
-copy \md\pti\pr\Baz1\SBz1DU.xml >null
-copy \md\pti\pr\Baz2\Baz2.xml >null
-copy \md\pti\pr\Baz2\SBz2DU.xml >null
-copy \md\pti\pr\scm\scm.xml >null
-copy \md\pti\pr\scm\SBz1.xml >null
-copy \md\pti\pr\scm\SBz2.xml >null
-copy \md\pti\pr\scm\SDu.xml >null
+copy \md\pti\pr\DU\DU.xml >NUL
+copy \md\pti\pr\Baz1\Baz1.xml >NUL
+copy \md\pti\pr\Baz1\SBz1DU.xml >NUL
+copy \md\pti\pr\Baz2\Baz2.xml >NUL
+copy \md\pti\pr\Baz2\SBz2DU.xml >NUL
+copy \md\pti\pr\scm\scm.xml >NUL
+copy \md\pti\pr\scm\SBz1.xml >NUL
+copy \md\pti\pr\scm\SBz2.xml >NUL
+copy \md\pti\pr\scm\SDu.xml >NUL
 echo All updated!
